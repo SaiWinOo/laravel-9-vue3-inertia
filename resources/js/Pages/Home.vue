243be679a9642d569f41494b105ce52b7e519088
saiwinoo <saiwinoo25@gmail.com>
2022-11-19 10:26:07 +0630

    vue_laravel_inertia

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>I am Home!</h1>
+        <h1>{{ name }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Home",
+    data() {
+        return {
+            name: "Sai Win Oo",
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
